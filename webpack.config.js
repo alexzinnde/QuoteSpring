9a3d95614a969b7dd973045d2d9f393201ac8831
Alexander Zinn <alexzinnde@gmail.com>
2021-05-20 08:48:47 -0400

    Fix webpack config to account for client folder move

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
-const SRC_DIR = path.join(__dirname, '/client/src');
-const DIST_DIR = path.join(__dirname, '/client/dist');
+const SRC_DIR = path.join(__dirname, '/server/client/src');
+const DIST_DIR = path.join(__dirname, '/server/client/dist');
 
 module.exports = {
   entry: `${SRC_DIR}/index.jsx`,
